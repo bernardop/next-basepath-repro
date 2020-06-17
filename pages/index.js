55248929631ed9e3af543299284f2a7662424cb2
Bernardo Pacheco <bernardo.pacheco@dominos.com>
2020-06-17 11:37:51 -0400

    Added conditions for reproducing "data/development.json" bug

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,34 @@
 import React from "react";
 import Link from "next/link";
 
-export default function Home() {
+export default function Home({ data }) {
   return (
-    <ul>
-      <li>
-        <Link href="/a">
-          <a>a</a>
-        </Link>
-      </li>
-      <li>
-        <Link href="/b">
-          <a>b</a>
-        </Link>
-      </li>
-    </ul>
+    <>
+      <ul>
+        <li>
+          <Link href="/a">
+            <a>a</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/b">
+            <a>b</a>
+          </Link>
+        </li>
+      </ul>
+
+      <h1>Hey, {data.name}</h1>
+    </>
   );
 }
+
+export const getServerSideProps = async () => {
+  const res = await fetch("https://swapi.dev/api/people/1/");
+  const data = await res.json();
+
+  return {
+    props: {
+      data,
+    },
+  };
+};
