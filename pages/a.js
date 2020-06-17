b054ee1c91bb1dd0ea8901e41f1889899e11d262
Bernardo Pacheco <bernardo.pacheco@dominos.com>
2020-06-17 14:18:45 -0400

    Updated example to check if the error happened in a page route

--- pages/a.js
+++ pages/a.js
@@ -1,12 +1,24 @@
 import Link from "next/link";
 
-export default function A() {
+export default function A({ data }) {
   return (
     <div>
-      <div>A</div>
+      <h1>A</h1>
+      <h2>Hey, {data.name}</h2>
       <Link href="/">
         <a>Back</a>
       </Link>
     </div>
   );
 }
+
+export const getServerSideProps = async () => {
+  const res = await fetch("https://swapi.dev/api/people/2/");
+  const data = await res.json();
+
+  return {
+    props: {
+      data,
+    },
+  };
+};
