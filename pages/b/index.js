08812099957365e41b394c2a4865a5bf8f30c0e4
Bernardo Pacheco <bernardo.pacheco@dominos.com>
2020-06-17 14:22:32 -0400

    Updated example to try in a catch-all route (error doesn't happen here)

---
+++ pages/b/index.js
@@ -0,0 +1,24 @@
+import Link from "next/link";
+
+export default function B({ data }) {
+  return (
+    <div>
+      <h1>B</h1>
+      <h2>Hey, {data.name}</h2>
+      <Link href="/">
+        <a>Back</a>
+      </Link>
+    </div>
+  );
+}
+
+export const getServerSideProps = async () => {
+  const res = await fetch("https://swapi.dev/api/people/3/");
+  const data = await res.json();
+
+  return {
+    props: {
+      data,
+    },
+  };
+};
